3b84d207b666f03e52e670f22b5d510e891887c6
singhdeewas2-glitch <singhdeewas2@gmail.com>
2026-04-13 12:04:25 +0530

    msg

--- backend/src/routes.mjs
+++ backend/src/routes.mjs
@@ -4,9 +4,7 @@ import { requireAuth } from './middleware/authMiddleware.mjs';
 import { getTransformations, getTrainers, getPlans } from './controller/publicController.mjs';
 
 const router = express.Router();
-router.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+router.get("/", (req, res) => {res.send("Hello World!")});
 router.post('/login', loginUser);
 router.post('/register', registerUser);
 router.get('/profile', requireAuth, getProfile);
